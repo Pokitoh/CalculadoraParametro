9d8696acf286f1491055eb3fa5669104d2a16137
Pokitoh <orlando1258831212@gmail.com>
2026-06-05 10:38:27 -0400

    commit

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,6 @@
 public class Calculadora {
 
+    // 2 parametros
     public int sumar(int a, int b) {
         return a + b;
     }
@@ -14,5 +15,31 @@ public class Calculadora {
 
     public int dividir(int a, int b) {
         return a / b;
+    }
+
+    // 3 parametros
+    public int sumar(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public int restar(int a, int b, int c) {
+        return a - b - c;
+    }
+
+    public int multiplicar(int a, int b, int c) {
+        return a * b * c;
+    }
+
+    // 4 parametros
+    public int sumar(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
+    public int restar(int a, int b, int c, int d) {
+        return a - b - c - d;
+    }
+
+    public int multiplicar(int a, int b, int c, int d) {
+        return a * b * c * d;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,24 @@ public class Main {
 
         Calculadora calc = new Calculadora();
 
-        System.out.println("Suma: "
-                + calc.sumar(10, 8));
+        // 2 parámetros
+        System.out.println("Suma (2): " + calc.sumar(7, 5));
+        System.out.println("Resta (2): " + calc.restar(14, 5));
+        System.out.println("Multiplicación (2): " + calc.multiplicar(10, 5));
+        System.out.println("División: " + calc.dividir(10, 5));
 
-        System.out.println("Resta: "
-                + calc.restar(10, 8));
+        System.out.println();
 
-        System.out.println("Multiplicación: "
-                + calc.multiplicar(10, 5));
+        // 3 parámetros
+        System.out.println("Suma (3): " + calc.sumar(16, 5, 2));
+        System.out.println("Resta (3): " + calc.restar(6, 5, 2));
+        System.out.println("Multiplicación (3): " + calc.multiplicar(10, 5, 2));
 
-        System.out.println("División: "
-                + calc.dividir(10, 5));
+        System.out.println();
+
+        // 4 parámetros
+        System.out.println("Suma (4): " + calc.sumar(10, 5, 2, 3));
+        System.out.println("Resta (4): " + calc.restar(10, 5, 2, 3));
+        System.out.println("Multiplicación (4): " + calc.multiplicar(10, 5, 2, 3));
     }
 }
